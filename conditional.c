a35944ee621b06e0baebacf59317fde62a308f58
Rakibul Hasan Rabbi <hasanrakibul1488@gmail.com>
2026-02-08 23:14:12 +0600

    Add files via upload

---
+++ conditional.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (){
+
+ int age;
+ printf("enter ager: ");
+ scanf("%d",&age);
+
+ if(age > 18){
+
+    printf("adult \n");
+    printf("they can vote\n");
+    printf("tthey c an vote\n");
+
+ }
+
+else{
+
+    printf("not adult \n");
+}
+return 0;
+
+}
